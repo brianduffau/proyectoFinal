2872b15f1e0a9d775c9daf5bbecfcddb3393a076
guidocaru <carugatiguido@gmail.com>
2022-05-02 21:28:38 -0300

    agrega firebase

--- app/src/main/java/com/example/proyectofinal/MainActivity.kt
+++ app/src/main/java/com/example/proyectofinal/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.proyectofinal
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val db = Firebase.firestore
+
+        // Create a new user with a first and last name
+        val user = hashMapOf(
+            "nombre" to "Ada",
+            "apellido" to "Lovelace",
+            "mail" to "ada@gmail.com",
+            "direccion" to "Rivadavia 133"
+        )
+
+// Add a new document with a generated ID
+        db.collection("usuarios")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "Usuario agregado con id : ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
+
+
     }
 }
