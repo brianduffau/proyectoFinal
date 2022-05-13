fa9c62416d83d4352c106e84d85a762f868e0b35
gabrielpaoli <gabrielpaoli@hotmail.com>
2022-05-12 21:12:57 -0300

    add professional profile files

---
+++ app/src/main/java/com/example/proyectofinal/fragments/ProfessionalProfile.kt
@@ -0,0 +1,33 @@
+package com.example.proyectofinal.fragments
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.proyectofinal.R
+import com.example.proyectofinal.viewModel.ProfessionalProfileViewModel
+
+class ProfessionalProfile : Fragment() {
+
+    companion object {
+        fun newInstance() = ProfessionalProfile()
+    }
+
+    private lateinit var viewModel: ProfessionalProfileViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.professional_profile, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(ProfessionalProfileViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/proyectofinal/viewModel/ProfessionalProfileViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.proyectofinal.viewModel
+
+import androidx.lifecycle.ViewModel
+
+class ProfessionalProfileViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
