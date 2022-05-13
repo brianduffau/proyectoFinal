a631a257f21d9990c3d571e83162abfba1d00f28
camibec <cami8bec@gmail.com>
2022-05-12 22:16:05 -0300

    modificacion build gradle

---
+++ app/src/main/java/com/example/proyectofinal/fragments/Map.kt
@@ -0,0 +1,35 @@
+package com.example.proyectofinal.fragments
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.proyectofinal.R
+import com.example.proyectofinal.databinding.ActivityMapsBinding
+import com.example.proyectofinal.viewModel.MapViewModel
+import com.google.android.gms.maps.GoogleMap
+
+class Map : Fragment() {
+
+    companion object {
+        fun newInstance() = Map()
+    }
+
+    private lateinit var viewModel: MapViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.map_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MapViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
