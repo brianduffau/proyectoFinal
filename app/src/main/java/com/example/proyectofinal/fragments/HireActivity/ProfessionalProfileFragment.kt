8d1375e6aa455bb891ec61c40d03a1eb36ba7c72
camibec <cami8bec@gmail.com>
2022-06-19 23:43:43 -0300

    photo in recyclers - details in pet

--- app/src/main/java/com/example/proyectofinal/fragments/HireActivity/ProfessionalProfileFragment.kt
+++ app/src/main/java/com/example/proyectofinal/fragments/HireActivity/ProfessionalProfileFragment.kt
@@ -32,6 +32,7 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
+import com.squareup.picasso.Picasso
 import java.util.*
 import kotlin.collections.ArrayList
 import kotlin.math.abs
@@ -100,6 +101,7 @@ class ProfessionalProfileFragment : Fragment(){
 
         professionalName.text = professional.name
         professionalType.text = professional.professionalType
+        Picasso.get().load(professional.img).fit().centerCrop().into(profileImage)
 
 
         setupHireButton()
